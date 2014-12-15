857443f7815ed3f28a4a23e05528e052fd6fe14e
Piyush Verma <github@piyushverma.net>
2014-12-15 14:29:12 +0530

    Handle gzip and deflate outputs

--- request.go
+++ request.go
@@ -2,11 +2,12 @@ package gottp
 
 import (
 	"compress/gzip"
+	"compress/zlib"
 	"net/http"
 	"strconv"
 	"strings"
-	//"compress/gzip"
-	utils "github.com/Simversity/gottp/utils"
+
+	"github.com/Simversity/gottp/utils"
 )
 
 //*http.Request, rw ResponseWriter
@@ -165,12 +166,21 @@ func (r *Request) Write(data interface{}) {
 		piped["index"] = r.PipeIndex
 		r.PipeOutput <- &piped
 	} else if strings.Contains(
+		r.Request.Header.Get("Accept-Encoding"), "deflate",
+	) {
+		r.Writer.Header().Set("Content-Encoding", "deflate")
+		gz := zlib.NewWriter(r.Writer)
+		defer gz.Close()
+		gz.Write(utils.Encoder(piped))
+
+	} else if strings.Contains(
 		r.Request.Header.Get("Accept-Encoding"), "gzip",
 	) {
 		r.Writer.Header().Set("Content-Encoding", "gzip")
 		gz := gzip.NewWriter(r.Writer)
 		defer gz.Close()
 		gz.Write(utils.Encoder(piped))
+
 	} else {
 		r.Writer.Write(utils.Encoder(piped))
 	}
