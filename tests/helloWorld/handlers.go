97ebcbe92a2f78de0732efe5527b1bf90fb1fb22
Piyush Verma <github@piyushverma.net>
2015-01-14 19:15:47 +0530

    Remove the tests. They were meaningless anyway

--- tests/helloWorld/handlers.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/Simversity/gottp"
-)
-
-func helloMessage(req *gottp.Request) {
-	req.Write("hello world")
-}
--- tests/helloWorld/urls.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/Simversity/gottp"
-)
-
-var urls = []*gottp.Url{
-	gottp.NewUrl("hello", "/hello/\\w{3,5}/?$", helloMessage),
-}
