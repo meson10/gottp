ab5414dce3c41a4f65d61ae630c511783991242c
alaypatel07 <alay1431@gmail.com>
2015-11-04 00:34:38 +0530

    Response to /urls now includes the list of methods implemented by each url

--- url_handler.go
+++ url_handler.go
@@ -5,7 +5,7 @@ type UrlHandler struct {
 	BaseHandler
 }
 
-var allUrlsMap = map[string]string{}
+var allUrlsMap = make(map[string]interface{})
 
 // Get implements the method for UrlHandler.
 // It would be an advantage to have methods
@@ -13,10 +13,12 @@ var allUrlsMap = map[string]string{}
 func (self *UrlHandler) Get(req *Request) {
 	if len(allUrlsMap) == 0 {
 		for _, url := range boundUrls {
-			allUrlsMap[url.name] = url.url
+			temp := make(map[string]interface{})
+			temp["url"] = url.url
+			temp["methodsImplemented"] = url.methodsImplemented
+			allUrlsMap[url.name] = temp
 		}
 	}
-
 	req.Write(allUrlsMap)
 	return
 }
