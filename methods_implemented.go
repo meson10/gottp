2482915ac2d1ed0ab3b3b9eab51bdd1a3f6d8d7b
alaypatel07 <alay1431@gmail.com>
2015-11-04 00:15:16 +0530

    Options method implemented correctly by default

--- methods_implemented.go
+++ methods_implemented.go
@@ -1,7 +1,7 @@
 package gottp
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 
 	"github.com/parnurzeal/gorequest"
@@ -14,39 +14,74 @@ func methondImplemented(u *Url) {
 	}
 	methodsSupported := []string{"GET", "PUT", "POST", "DELETE", "HEAD", "OPTIONS", "PATCH"}
 	url := "http://" + Address() + u.url
-	fmt.Println(url)
 	client := gorequest.New()
 	for _, method := range methodsSupported {
 		switch method {
 		case "GET":
-			resp, _, _ := client.Get(url).End()
-			if resp.StatusCode != http.StatusNotImplemented {
-				u.methodsImplemented = append(u.methodsImplemented, method)
+			resp, _, err := client.Get(url).End()
+			if err == nil {
+				if resp.StatusCode == http.StatusOK {
+					var data map[string]int
+					json.NewDecoder(resp.Body).Decode(&data)
+					if data["status"] != 501 && data["status"] != 0 {
+						u.methodsImplemented = append(u.methodsImplemented, method)
+					}
+				}
 			}
 		case "POST":
-			resp, _, _ := client.Post(url).End()
-			if resp.StatusCode != http.StatusNotImplemented {
-				u.methodsImplemented = append(u.methodsImplemented, method)
+			resp, _, err := client.Post(url).End()
+			if err == nil {
+				if resp.StatusCode == http.StatusOK {
+					var data map[string]int
+					json.NewDecoder(resp.Body).Decode(&data)
+					if data["status"] != 501 && data["status"] != 0 {
+						u.methodsImplemented = append(u.methodsImplemented, method)
+					}
+				}
 			}
 		case "PUT":
-			resp, _, _ := client.Put(url).End()
-			if resp.StatusCode != http.StatusNotImplemented {
-				u.methodsImplemented = append(u.methodsImplemented, method)
+			resp, _, err := client.Put(url).End()
+			if err == nil {
+				if resp.StatusCode == http.StatusOK {
+					var data map[string]int
+					json.NewDecoder(resp.Body).Decode(&data)
+					if data["status"] != 501 && data["status"] != 0 {
+						u.methodsImplemented = append(u.methodsImplemented, method)
+					}
+				}
 			}
 		case "DELETE":
-			resp, _, _ := client.Delete(url).End()
-			if resp.StatusCode != http.StatusNotImplemented {
-				u.methodsImplemented = append(u.methodsImplemented, method)
+			resp, _, err := client.Delete(url).End()
+			if err == nil {
+				if resp.StatusCode == http.StatusOK {
+					var data map[string]int
+					json.NewDecoder(resp.Body).Decode(&data)
+					if data["status"] != 501 && data["status"] != 0 {
+						u.methodsImplemented = append(u.methodsImplemented, method)
+					}
+				}
 			}
 		case "HEAD":
-			resp, _, _ := client.Head(url).End()
-			if resp.StatusCode != http.StatusNotImplemented {
-				u.methodsImplemented = append(u.methodsImplemented, method)
+			resp, _, err := client.Head(url).End()
+			if err == nil {
+				if resp.StatusCode == http.StatusOK {
+					var data map[string]int
+					json.NewDecoder(resp.Body).Decode(&data)
+					if data["status"] != 501 && data["status"] != 0 {
+						u.methodsImplemented = append(u.methodsImplemented, method)
+					}
+				}
 			}
 		case "PATCH":
-			resp, _, _ := client.Patch(url).End()
-			if resp.StatusCode != http.StatusNotImplemented {
-				u.methodsImplemented = append(u.methodsImplemented, method)
+			resp, _, err := client.Patch(url).End()
+			if err == nil {
+				if resp.StatusCode == http.StatusOK {
+					var data map[string]int
+					json.NewDecoder(resp.Body).Decode(&data)
+					if data["status"] != 501 && data["status"] != 0 {
+						u.methodsImplemented = append(u.methodsImplemented, method)
+					}
+				}
 			}
 		}
 	}
